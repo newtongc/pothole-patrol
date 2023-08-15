144583f8c76760716e64abd1888fcfcb17e62219
Ethan Smith <ethansmith297@gmail.com>
2023-08-15 10:24:39 -0400

    started cloudinary work

--- vue/src/main.js
+++ vue/src/main.js
@@ -3,7 +3,13 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 import axios from 'axios'
-
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: 'dd6n4zinx', 
+  api_key: '843273134359491', 
+  api_secret: 'qiAT6VTVhjQadsgL5Uqs6CM5MTI' 
+});
 Vue.config.productionTip = false
 
 axios.defaults.baseURL = process.env.VUE_APP_REMOTE_API;
